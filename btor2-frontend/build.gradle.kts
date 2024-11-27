9146bc513fb1c20c3211c2b40e6a9ac64d075681
Szabó Éva Mária <szaboevamaria@edu.bme.hu>
2024-11-27 23:55:32 +0100

    Step1

--- btor2-frontend/build.gradle.kts
+++ btor2-frontend/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    antlr
 }
 
 group = "org.example"
@@ -12,6 +13,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    antlr("org.antlr:antlr4:4.9.2")
+    implementation("org.antlr:antlr4-runtime:4.9.2")
+    implementation("hu.bme.mit.theta:theta-xcfa:6.8.1")
 }
 
 tasks.test {
@@ -19,9 +23,31 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(17)
 }
 
 application {
     mainClass.set("MainKt")
+}
+apply<AntlrPlugin>()
+
+open class GenerateGrammarExtension(project: Project) {
+    var packageName: String = "org.example.${project.name.replace("-", ".")}.dsl.gen"
+}
+
+tasks {
+    val grammar = extensions.create<GenerateGrammarExtension>("grammar", project)
+
+    named("generateGrammarSource", AntlrTask::class) {
+        val packageName = grammar.packageName
+        val directoryName = packageName.replace(".", File.separator)
+        val pacakgeQualifiedOutputDirectory = outputDirectory.resolve(directoryName)
+        outputDirectory = pacakgeQualifiedOutputDirectory
+
+        arguments.addAll(listOf("-package", packageName, "-Werror", "-visitor"))
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("generateGrammarSource")
 }
--- btor2-frontend/src/main/kotlin/Main.kt
+++ btor2-frontend/src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import org.antlr.v4.runtime.BailErrorStrategy
+import org.antlr.v4.runtime.CharStreams
+import org.antlr.v4.runtime.CommonTokenStream
+import org.example.btor2.frontend.dsl.gen.Btor2Lexer
+import org.example.btor2.frontend.dsl.gen.Btor2Parser
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val visitor = Btor2Visitor()
+    val c = """
+        1 sort bitvec 3
+        2 zero 1
+        3 state 1
+        4 init 1 3 2    
+5 one 1
+6 add 1 3 5
+7 next 1 3 6
+8 ones 1
+9 sort bitvec 1
+10 eq 9 3 8
+11 bad 10
+    """.trimIndent()
+
+    val input = CharStreams.fromString(c)
+    val lexer = Btor2Lexer(input)
+    val tokens = CommonTokenStream(lexer)
+    val parser = Btor2Parser(tokens)
+    parser.errorHandler = BailErrorStrategy()
+    val context = parser.btor2()
+
+    context.accept(visitor)
 }
